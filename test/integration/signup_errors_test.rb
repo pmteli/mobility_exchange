e8a086a86ba2e0ec0899272d07aa3ffcab5e6120
pmteli <prakash0450@gmail.com>
2026-09-16 15:52:28 -0700

    Place signup errors below title with pink error styling

--- test/integration/signup_errors_test.rb
+++ test/integration/signup_errors_test.rb
@@ -9,7 +9,7 @@ class SignupErrorsTest < ActionDispatch::IntegrationTest
     end
     assert_response :too_many_requests
     assert_equal "3600", response.headers["Retry-After"]
-    assert_select "form #signup-errors", text: /Too many signup attempts/
+    assert_select ".auth-form #signup-errors", text: /Too many signup attempts/
     assert_select "input[name=first_name][value=Alex]"
   ensure
     store.define_singleton_method(:increment, original) if original
@@ -21,12 +21,12 @@ class SignupErrorsTest < ActionDispatch::IntegrationTest
       post registration_path, params: attrs
     end
     assert_response :unprocessable_entity
-    assert_select "form #signup-errors[role=alert] li", minimum: 1
+    assert_select ".auth-form #signup-errors[role=alert] li", minimum: 1
     %i[first_name last_name email].each { |field| assert_select "input[name='#{field}'][value='#{attrs[field]}']" }
     assert_select "input[value=donor][checked]"
     assert_select "input[value=recipient][checked]"
     assert_select "input[type=password][value]", count: 0
-    assert_select "fieldset + #signup-errors"
+    assert_select "h1 + #signup-errors.signup-error-summary"
   end
 
   test "duplicate email and missing roles show errors inside signup form" do
@@ -34,9 +34,9 @@ class SignupErrorsTest < ActionDispatch::IntegrationTest
     attrs = {first_name: "Alex", last_name: "User", email: existing.email, password: "12345678", password_confirmation: "12345678", community_roles: ["donor"]}
     post registration_path, params: attrs
     assert_response :unprocessable_entity
-    assert_select "form #signup-errors", text: /Email has already been taken/
+    assert_select ".auth-form #signup-errors", text: /Email has already been taken/
     post registration_path, params: attrs.merge(community_roles: [])
     assert_response :unprocessable_entity
-    assert_select "form #signup-errors", text: /Select at least one role/
+    assert_select ".auth-form #signup-errors", text: /Select at least one role/
   end
 end
